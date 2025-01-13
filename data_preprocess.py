7e64b186269c8e67e11524d46fdb377bded41caf
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2025-01-13 18:45:20 +0100

    Spiegazione del codiec

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,7 +3,10 @@ from PIL import Image
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 
-
+# Funzione per creare le cartelle di output
+# crea le cartelle di output
+# base_path: percorso della cartella principale
+# return: percorso delle cartelle AI e REAL
 def create_output_folders(base_path):
     processed_path = os.path.join(base_path, "DatasetTraining(Processed)")
     ai_path = os.path.join(processed_path, "AI")
@@ -14,7 +17,9 @@ def create_output_folders(base_path):
 
     return ai_path, real_path
 
-
+# Funzione per ridimensionare e salvare le immagini
+# image_path: percorso dell'immagine da ridimensionare
+# output_folder: percorso della cartella di output
 def resize_and_save(image_path, output_folder):
     try:
         with Image.open(image_path) as img:
@@ -25,13 +30,16 @@ def resize_and_save(image_path, output_folder):
     except Exception as e:
         print(f"Error processing {image_path}: {e}")
 
-
+# Funzione per elaborare una cartella
+# input_folder: percorso della cartella di input
+# output_folder: percorso della cartella di output
+# folder_name: nome della cartella interna a output_folder
 def process_folder(input_folder, output_folder, folder_name):
     image_paths = [os.path.join(input_folder, img) for img in os.listdir(input_folder) if
                    img.lower().endswith(('png', 'jpg', 'jpeg'))]
 
-    with tqdm(total=len(image_paths), desc=f"Processing {folder_name}", unit="file") as pbar:
-        with ThreadPoolExecutor() as executor:
+    with tqdm(total=len(image_paths), desc=f"Processing {folder_name}", unit="file") as pbar: # barra di progresso
+        with ThreadPoolExecutor() as executor:                                                # esecuzione in parallelo
             futures = {executor.submit(resize_and_save, image_path, output_folder): image_path for image_path in
                        image_paths}
             for future in as_completed(futures):
