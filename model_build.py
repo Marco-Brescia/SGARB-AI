03bdf5c288f3fdbf6b3a95b06b526a4fa3ca12fa
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2025-01-13 19:07:25 +0100

    Aggiornata funzione di loss
    Co-authored-by: Marco-Brescia <marcbresc03@gmail.com>
    Co-authored-by: Francesco-Faiella <faiellafrancesco2003@gmail.com>

--- model_build.py
+++ model_build.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.applications import ResNet50
+from tensorflow.keras import backend as K
 
 # Funzione per costruire il modello
 # hp: oggetto HyperParameters
@@ -9,7 +10,6 @@ from tensorflow.keras.applications import ResNet50
 # Return: modello costruito
 # La funzione costruisce un modello con un'architettura ResNet50 e un classificatore custom
 # L'ottimizzatore, la funzione di loss e le metriche sono definite all'interno della funzione
-
 def build_model(hp, img_width, img_height):
     base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
     base_model.trainable = False
@@ -22,10 +22,6 @@ def build_model(hp, img_width, img_height):
         layers.Dense(1, activation='sigmoid')
     ])
 
-    def weighted_loss(y_true, y_pred):
-        weight = tf.where(tf.equal(y_true, 0), 10.0, 1.0)
-        return tf.reduce_mean(weight * tf.keras.losses.binary_crossentropy(y_true, y_pred))
-
     model.compile(
         optimizer=hp.Choice('optimizer', values=['adam', 'sgd']),
         loss=weighted_loss,
@@ -33,3 +29,25 @@ def build_model(hp, img_width, img_height):
     )
 
     return model
+
+def weighted_loss(y_true, y_pred):
+     # Peso per la classe AI (etichetta 0)
+    weight_for_0 = 1.5  # Maggiore peso per la classe AI
+    weight_for_1 = 1.0  # Ponderazione per la classe REAL (etichetta 1)
+
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.clip_by_value(y_pred, K.epsilon(), 1.0 - K.epsilon())
+
+    # Calcola il cross-entropy per ciascun campione
+    cross_entropy = - (y_true * tf.math.log(y_pred) + (1 - y_true) * tf.math.log(1 - y_pred))
+
+    # Aggiungi i pesi
+    weight = y_true * weight_for_0 + (1 - y_true) * weight_for_1
+    weighted_loss = cross_entropy * weight
+
+    return tf.reduce_mean(weighted_loss)
+
+#vecchia funzione di loss
+# def weighted_loss(y_true, y_pred):
+#    weight = tf.where(tf.equal(y_true, 0), 10.0, 1.0)
+#    return tf.reduce_mean(weight * tf.keras.losses.binary_crossentropy(y_true, y_pred))
