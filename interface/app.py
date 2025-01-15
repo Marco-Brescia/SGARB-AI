7b20cbbf90561afd350da3356a527feb1f184245
GiuseppeGambardella <gagiuseppe29@gmail.com>
2025-01-15 19:10:09 +0100

    modificato layout demo e modificati script python e javascript per il funzionamento sul nuovo layout

--- interface/app.py
+++ interface/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 import os
-import random  # Per simulare la predizione
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
