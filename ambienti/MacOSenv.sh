e33d096f52dfc6432da437c096cfeef65f9f7984
GiuseppeGambardella <gagiuseppe29@gmail.com>
2025-01-19 15:14:32 +0100

    Risolto problema dello script se conda non era installato

--- ambienti/MacOSenv.sh
+++ ambienti/MacOSenv.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
 # Verifica se Anaconda è già installato
-if ! command -v conda &> /dev/null
-then
+if ! command -v conda &> /dev/null; then
     echo "Anaconda non trovato, procedo con l'installazione..."
+
+    # Controlla se `curl` è installato
+    if ! command -v curl &> /dev/null; then
+        echo "Errore: il comando 'curl' non è installato. Installalo e riprova."
+        exit 1
+    fi
 
     # Scarica il file di installazione di Anaconda
     echo "Scaricando Anaconda..."
-    wget --progress=bar:force https://repo.anaconda.com/archive/Anaconda3-2024.06-1-MacOSX-arm64.sh -O anaconda_installer.sh
+    curl -# -o anaconda_installer.sh https://repo.anaconda.com/archive/Anaconda3-2024.06-1-MacOSX-arm64.sh
 
     # Rendi eseguibile il file di installazione
     chmod +x anaconda_installer.sh
 
-    # Esegui l'installazione
-    ./anaconda_installer.sh -b -p $HOME/anaconda3
+    # Esegui l'installazione solo se la directory non esiste
+    if [ -d "$HOME/anaconda3" ]; then
+        echo "Anaconda è già presente in $HOME/anaconda3. Usa l'opzione -u per aggiornare manualmente."
+    else
+        ./anaconda_installer.sh -b -p $HOME/anaconda3
+    fi
 
-    # Aggiungi Anaconda al PATH
-    echo "Aggiungendo Anaconda al PATH..."
-    echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> ~/.bash_profile
-    source ~/.bash_profile
+    # Verifica se la riga non è già presente in ~/.zshrc
+    if ! grep -q 'export PATH="$HOME/anaconda3/bin:$PATH"' ~/.zshrc; then
+        # Aggiungi la riga al file ~/.zshrc
+        echo 'export PATH="$HOME/anaconda3/bin:$PATH"' | sudo tee -a ~/.zshrc > /dev/null
+    fi
 
-    echo "Anaconda installato con successo!"
-else
-    echo "Anaconda è già installato!"
+    # Ricarica il file ~/.zshrc per applicare le modifiche
+    source ~/.zshrc
+
+    rm anaconda_installer.sh
+
+    # Chiudi la console e riaprila
+    echo "Per applicare le modifiche al PATH, verrà aperto un altro terminale."
+    # Dopo aver aggiornato ~/.zshrc o ~/.bash_profile
+    osascript -e "tell application \"Terminal\" to do script \"cd '$(pwd)' && './MacOSenv.sh'\""
+    exit 0
 fi
 
 # Verifica se Conda è correttamente installato
-if command -v conda &> /dev/null
-then
+if command -v conda &> /dev/null; then
     echo "Conda è pronto per l'uso!"
 else
     echo "Errore: Conda non è stato installato correttamente."
