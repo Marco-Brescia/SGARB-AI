f7a7c8f1363f080089061748c1e4c5317dba4dec
FaiellaFrancesco <faiellafrancesco2003@gmail.com>
2025-01-20 16:37:14 +0100

    tolta print e blocchi che non esistevano nello script js

--- interface/static/script.js
+++ interface/static/script.js
@@ -50,7 +50,7 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
                 HumanConfidence.innerHTML = '';
                 blockRight.classList.add('inactive');
                 blockRight.classList.remove('active');
-                percIA.innerHTML = `${confidence}`;
+
             } else {
                 AiImage.classList.add('active_result');
                 animateConfidence(HumanConfidence, confidence);
@@ -60,14 +60,14 @@ document.getElementById('upload-form').addEventListener('submit', async (e) => {
                 AiConfidence.innerHTML = '';
                 blockLeft.classList.add('inactive');
                 blockLeft.classList.remove('active');
-                percReal.innerHTML = `${confidence}`;
+
             }
         } else {
             alert('Errore nella classificazione');
         }
     } catch (err) {
         console.error('Errore:', err);
-        print(err);
+
         alert('Errore nella comunicazione con il server.');
     }
 });
