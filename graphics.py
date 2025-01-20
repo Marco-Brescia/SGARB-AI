be34e7b661c9401788eb9592f11fa10cc7831854
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2025-01-20 15:17:59 +0100

    Modifiche addestramento modello in base alla specificity
    Co-authored-by: Marco-Brescia <marcbresc03@gmail.com>
    Co-authored-by: Francesco-Faiella <faiellafrancesco2003@gmail.com>

--- graphics.py
+++ graphics.py
@@ -1,31 +1,32 @@
 import os
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
-def plot_metrics(history, accuracy, recall, precision, output_dir):
-    # Grafico per accuracy e recall
+def plot_metrics(history, accuracy, recall, precision, specificity, y_test, y_probs, output_dir):
+    # Grafico per accuracy e specificity3
     plt.figure(figsize=(10, 6))
     if 'accuracy' in history.history and 'val_accuracy' in history.history:
         plt.plot(history.history['accuracy'], label='Training Accuracy', linestyle='--', color='blue')
         plt.plot(history.history['val_accuracy'], label='Validation Accuracy', linestyle='-', color='blue')
-    if 'recall' in history.history and 'val_recall' in history.history:
-        plt.plot(history.history['recall'], label='Training Recall', linestyle='--', color='green')
-        plt.plot(history.history['val_recall'], label='Validation Recall', linestyle='-', color='green')
-    plt.title('Accuracy and Recall (Training vs Validation)', fontsize=16)
+    if 'specificity' in history.history and 'val_specificity' in history.history:
+        plt.plot(history.history['specificity'], label='Training Specificity', linestyle='--', color='purple')
+        plt.plot(history.history['val_specificity'], label='Validation Specificity', linestyle='-', color='purple')
+    plt.title('Accuracy and Specificity (Training vs Validation)', fontsize=16)
     plt.xlabel('Epochs', fontsize=12)
     plt.ylabel('Metrics', fontsize=12)
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, 'accuracy_recall_plot.png'))
+    plt.savefig(os.path.join(output_dir, 'accuracy_specificity_plot.png'))
     plt.close()
 
-    # Grafico per accuracy, recall e precision
-    metrics = ['Accuracy', 'Recall', 'Precision']
-    values = [accuracy, recall, precision]
+    # Grafico per accuracy, recall, precision e specificity3
+    metrics = ['Accuracy', 'Recall', 'Precision', 'Specificity']
+    values = [accuracy, recall, precision, specificity]
 
     plt.figure(figsize=(10, 6))
-    plt.bar(metrics, [value * 100 for value in values], color=['blue', 'green', 'orange'])
-    plt.title('Comparison of Accuracy, Recall, and Precision', fontsize=16)
+    plt.bar(metrics, [value * 100 for value in values], color=['blue', 'green', 'orange', 'purple'])
+    plt.title('Comparison of Accuracy, Recall, Precision and Specificity', fontsize=16)
     plt.ylabel('Percentage', fontsize=12)
     plt.ylim(0, 100)
 
@@ -33,7 +34,7 @@ def plot_metrics(history, accuracy, recall, precision, output_dir):
         plt.text(i, v * 100 + 2, f"{v*100:.2f}%", ha='center', color='black', fontsize=12)
 
     plt.tight_layout()
-    plt.savefig(os.path.join(output_dir, 'accuracy_recall_precision_plot.png'))
+    plt.savefig(os.path.join(output_dir, 'accuracy_recall_precision_specificity_plot.png'))
     plt.close()
 
     # Grafico per training e validation loss
@@ -50,3 +51,21 @@ def plot_metrics(history, accuracy, recall, precision, output_dir):
     plt.savefig(os.path.join(output_dir, 'training_validation_loss_plot.png'))
     plt.close()
 
+    # Calcolare la curva ROC e l'area sotto la curva (AUC)
+    fpr, tpr, thresholds = roc_curve(y_test, y_probs)
+    roc_auc = auc(fpr, tpr)
+
+    # Grafico ROC
+    plt.figure(figsize=(10, 6))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')  # linea diagonale
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate', fontsize=12)
+    plt.ylabel('True Positive Rate', fontsize=12)
+    plt.title('Receiver Operating Characteristic (ROC)', fontsize=16)
+    plt.legend(loc='lower right')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, 'roc_curve.png'))
+    plt.close()
